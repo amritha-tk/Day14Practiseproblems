1c05601f03519a2ec1f11a1d1148d490e0029d00
amritha-tk <amrithatk95@gmail.com>
2020-06-09 19:20:28 +0530

    Modified

--- Passwordregex.sh
+++ Passwordregex.sh
@@ -7,7 +7,11 @@ length=$( echo -n $password | wc -m )
 echo $length
 
 if [[ $length -ge 8 ]]
+# && [[ $password == +[A-Z]+ ]] && [[ $password == +[0-9]+ ]] && [[ $password == +[a-z]+ ]] && [[ $password == +[^a-zA-Z0-9]+ ]]
 then
+if [[ $(echo "$password" | awk '/[a-z]/ && /[A-Z]/ && /[0-9]/ && /[^a-zA-z0-9]/ ') ]]
+then
+echo "Valid"
        regex="^[A-Za-z0-9]*[^a-zA-Z0-9]{1}[a-zA-Z0-9]*$"
        if [[ $password =~ $regex ]]
        then
@@ -16,6 +20,9 @@ then
           echo "Invalid"
 
         fi
+else
+echo "invalid"
+fi
 else
     echo "Password too short"
 fi
